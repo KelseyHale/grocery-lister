9c2547c21b67696e72eb3ab74878194a6b70dd7a
Kelsey Hale <kh.khale@gmail.com>
2015-08-03 14:38:16 -0400

    add new list item

--- backend/app/controllers/api/v1/items_controller.rb
+++ backend/app/controllers/api/v1/items_controller.rb
@@ -10,4 +10,22 @@ class Api::V1::ItemsController < ApplicationController
 
     render json: @item
   end
+
+  def create
+    @item = Item.new(name: item_params[:name])
+
+    if @item.save
+      render json: @item,
+      status: :created,
+      location: [:api, :v1, item]
+    else
+      render json: { errors: item.errors }, status: :unprocessable_entity
+    end
+  end
+
+  protected
+
+  def item_params
+    params.require(:item).permit(:name)
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show]
+      resources :items, only: [:index, :show, :create, :new]
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
