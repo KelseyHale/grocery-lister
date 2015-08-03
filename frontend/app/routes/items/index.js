e2fb8ddfe84d1e1964f7682c211c1e1d6c22ce09
Kelsey Hale <kh.khale@gmail.com>
2015-08-03 14:37:51 -0400

    add new list item

--- frontend/app/routes/items/index.js
+++ frontend/app/routes/items/index.js
@@ -3,5 +3,19 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('item');
-  }
+  },
+  actions: {
+        createItem: function(newItem) {
+            // Create the new Todo model
+            var item = this.store.createRecord('item', {
+                name: newItem
+            });
+
+            // Clear the "New Todo" text field
+            this.controllerFor('items').set('newItem', '');
+
+            // Save the new model
+            item.save();
+        }
+    }
 });
